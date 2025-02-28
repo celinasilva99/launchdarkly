088b09317c24444eb2de659a1dd900a9d3e45699
celinasilva99 <36280174+celinasilva99@users.noreply.github.com>
2025-02-28 20:32:13 +0000

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Deploying with LaunchDarkly Project Key: ${PROJECT_KEY} and Feature Flag Key: ${FEATURE_FLAG_KEY}"
-
-# Example API call to LaunchDarkly
-curl -H "Authorization: Bearer $LAUNCHDARKLY_API_KEY" \
-     -X POST "https://app.launchdarkly.com/api/v2/flags/$PROJECT_KEY/$FEATURE_FLAG_KEY"
